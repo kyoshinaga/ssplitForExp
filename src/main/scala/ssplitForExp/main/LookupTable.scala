869c5231f010f99ba079bed987811d4f7e6796b1
tomeken-yoshinaga <kenta-yoshinaga@aist.go.jp>
2017-01-16 13:59:30 +0900

    [update] Modify Lookup table. "UNKWNOWN" is masked as 0.

--- src/main/scala/ssplitForExp/main/LookupTable.scala
+++ src/main/scala/ssplitForExp/main/LookupTable.scala
@@ -7,17 +7,16 @@ import scala.collection.mutable
 
 class LookupTable(s: Set[String]) {
 
-  private val key2id:mutable.Map[String, Int] = mutable.Map[String, Int]("UNKNOWN" -> 1,"\n" -> 2)
-  private val id2key:mutable.Map[String, String] = mutable.Map[String, String]("1" -> "UNKNOWN", "2" -> "\n")
+  private val key2id:mutable.Map[String, Int] = mutable.Map[String, Int]("UNKNOWN" -> 0,"\n" -> 1, " " -> 2)
+  private val id2key:mutable.Map[String, String] = mutable.Map[String, String]("0" -> "UNKNOWN", "1" -> "\n", "2" -> " ")
 
   private def maxId(): Int = key2id.values.max
 
   private def addMember(): Unit = s.map{x =>
     key2id.getOrElse(x, -1) match {
-      case id:Int if id == -1 => {
+      case id:Int if id == -1 =>
         id2key((maxId + 1).toString) = x
         key2id(x) = maxId + 1
-      }
       case _ =>
     }
   }
@@ -26,10 +25,9 @@ class LookupTable(s: Set[String]) {
 
   def appendMember(member: Set[String]): Unit = member.map{x =>
     key2id.getOrElse(x, -1) match {
-      case id:Int if id == -1 => {
+      case id:Int if id == -1 =>
         id2key((maxId + 1).toString) = x
         key2id(x) = maxId + 1
-      }
       case _ =>
     }
   }
