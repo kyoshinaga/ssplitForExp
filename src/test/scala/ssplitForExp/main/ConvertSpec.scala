4ad84f6931cc35bef382f0c4557737191b57de2d
tomeken-yoshinaga <kenta-yoshinaga@aist.go.jp>
2017-03-27 11:36:36 +0900

    [delete] test file.

--- src/test/scala/ssplitForExp/main/ConvertSpec.scala
+++ src/test/scala/ssplitForExp/main/ConvertSpec.scala
@@ -9,11 +9,12 @@ import org.scalatest.{FlatSpec, Matchers}
 class ConvertSpec extends FlatSpec with Matchers{
 
   def findPath(localPath:String): String = getClass.getClassLoader.getResource(localPath).getPath
-
+/*
   "apply" should "open XML file and return instance" in {
     val corpus = Convert(findPath("./OC01_00001.xml"))
 
     corpus.BIOFullCorpus.length should be > 0
   }
+  */
 
 }
